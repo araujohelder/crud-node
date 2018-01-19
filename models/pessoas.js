a8711d34a3582167dae73f7639550fc9edad6158
Helder Araujo <heldersilvaaraujo@gmail.com>
2018-01-19 12:07:46 -0200

    first commit

---
+++ models/pessoas.js
@@ -0,0 +1,43 @@
+const findAll = (connection) => {
+    return new Promise((resolve, reject) => {
+        connection.query('select * from pessoas', (err, results) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(results)
+            }
+        })
+    })
+}
+
+const deleteOne = (connection, id) => {
+    return new Promise((resolve, reject) => {
+        connection.query('delete from pessoas where id =' +id+ ' limit 1', (err, results) => {
+            if (err) {
+                console.log(err)
+                reject(err)
+            } else {
+                resolve(results)
+            }
+        })
+    })
+}
+
+const create = (connection, data) => {
+    return new Promise((resolve, reject) => {
+        connection.query(`insert into pessoas(nome, nascimento, cargo) values('${data.nome}','${data.nascimento}','${data.cargo}');`, (err) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve()
+            }
+        })
+
+    })
+}
+
+module.exports = {
+    findAll,
+    deleteOne,
+    create
+}
